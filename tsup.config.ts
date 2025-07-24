37ab37ff4163b4859c0684bb9537f1d1c49663ca
SAn4Es-TV <sanes328@gmail.com>
2025-07-24 20:54:16 +0300

    keepAlive

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,12 @@
 import { type Options, defineConfig } from "tsup";
+import { copyFileSync } from 'fs'
 
 export default defineConfig((options: Options) => ({
   entryPoints: ["src/**/*.ts"],
   clean: true,
+  onSuccess() {
+      copyFileSync('src/keepAlive.js', 'dist/keepAlive.js')  // ✅
+  },
   format: "cjs",
   ...options,
 }));
